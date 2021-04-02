411d9e7df730745ef359e93db35ff5ca82867442
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-04-02 09:35:57 -0700

    initial page set up in html files, line by line plan laid out in js files

---
+++ public/dostuff.js
@@ -0,0 +1,15 @@
+//get current user from local storage
+//show current user info on page 
+
+// sendNewDrinkBtn will get (GET /:id) 
+
+// newFavBtn will hit (PUT /:) from url and get input from field to send as new drink
+// save new returned user info to local strage 
+// refresh page with updated user info
+
+//getUserFavsBtn will hit (GET) all and show all users name & fav in a table
+//will need to map through returned array and append table to page in empty div allUserFavsList
+
+//deleteUserBtn will hit (DELETE /:id)
+//will append "your account has been removed" line to page with timeout 
+//then redirect to home page to sign in 
---
+++ public/drinkHome.js
@@ -0,0 +1,3 @@
+//sign up user by hitting db POST endpoint
+//save returned new user to local storage
+//redirect to Do Stuff Page with user ID in url 
